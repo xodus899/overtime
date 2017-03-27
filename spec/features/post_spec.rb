60bd35ed4b9e7ed0cb6d3ac934b8d4a0ed09c1b1
Christopher Justice <justcd@mail.broward.edu>
2017-03-26 20:36:23 -0400

    added hours requested to table

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,7 +5,7 @@ describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
 
   let(:post) do
-    Post.create(date: Date.today, rationale: "some content", user_id: user.id)
+    Post.create(date: Date.today, rationale: "some content", user_id: user.id, overtime_request: 3.5)
   end
 
   before do
@@ -35,7 +35,7 @@ describe 'navigate' do
 
     it 'has a scope so that only post creators can see their posts' do
       other_user = User.create(first_name: "wonder", last_name: "bread", email: "wonderbread@mail.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
       
@@ -58,7 +58,7 @@ describe 'navigate' do
       delete_user = FactoryGirl.create(:user)
       login_as(delete_user, :scope => :user)
 
-      post_to_delete = Post.create(date: Date.today, rationale: "Some content", user_id: delete_user.id)
+      post_to_delete = Post.create(date: Date.today, rationale: "Some content", user_id: delete_user.id, overtime_request: 3.5)
 
 
       visit posts_path
@@ -80,14 +80,16 @@ describe 'navigate' do
     it 'can be created from new form page' do
     fill_in 'post[date]', with: Date.today
     fill_in 'post[rationale]', with: "Some rationale"
-    click_on "Save"
+    fill_in 'post[overtime_request]', with: 4.5
+    
 
-    expect(page).to have_content("Some rationale")
+    expect { click_on "Save" }.to change(Post, :count).by(1)
   	end
 
   	it 'will have a user associated it' do
   		fill_in 'post[date]', with: Date.today
     	fill_in 'post[rationale]', with: "User Association"
+      fill_in 'post[overtime_request]', with: 4.5
     	click_on "Save"
 
     	expect(User.last.posts.last.rationale).to eq("User Association")
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,10 +9,18 @@ RSpec.describe Post, type: :model do
   		
   		expect(@post).to be_valid
   	end
-  	it "cannot be crated without a date and raionale" do
+  	it "cannot be crated without a date and raionale and overtime_request" do
   		@post.date = nil
   		@post.rationale = nil
+      @post.overtime_request = nil
   		expect(@post).to_not be_valid
   	end
+
+    it 'has an overtime_request greater than 0.0' do
+      @post.overtime_request = 0.0
+
+      expect(@post).to_not be_valid
+
+    end
   end
 end
