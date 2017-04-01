848b398f4b957305af5680ca807fbb2dd1e62679
Christopher Justice <justcd@mail.broward.edu>
2017-03-31 20:36:16 -0400

    live data admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ puts "1 admin_user created"
 
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} XOXO pour-over food truck, slow-carb four dollar toast meh keytar banh mi hot chicken. Hell of umami narwhal hashtag, edison bulb normcore organic sartorial roof party DIY aesthetic fap mlkshk taxidermy cred. Photo booth mlkshk meditation distillery, succulents cred hella pork belly food truck. Keytar hashtag skateboard, occupy shabby chic post-ironic pour-over yr pickled edison bulb everyday carry vape fap chicharrones. Paleo kitsch chartreuse pickled, tattooed chicharrones distillery pabst hella hexagon gentrify vaporware. Knausgaard venmo letterpress, tumblr put a bird on it squid wayfarers shabby chic pork belly butcher lomo fashion axe semiotics. Tattooed succulents poutine 3 wolf moon.", user_id: @user.id, overtime_request: 2.5)
 end
  puts "100 posts have been created"
 
