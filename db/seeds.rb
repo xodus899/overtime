adafb6dbc4e76582685b9833accbd0d849bea433
Christopher Justice <justcd@mail.broward.edu>
2017-04-08 11:15:18 -0400

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,6 @@ AdminUser.create(email: "admin@mail.com",
 
 puts "1 admin_user created"
 
-
-100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} XOXO pour-over food truck, slow-carb four dollar toast meh keytar banh mi hot chicken. Hell of umami narwhal hashtag, edison bulb normcore organic sartorial roof party DIY aesthetic fap mlkshk taxidermy cred. Photo booth mlkshk meditation distillery, succulents cred hella pork belly food truck. Keytar hashtag skateboard, occupy shabby chic post-ironic pour-over yr pickled edison bulb everyday carry vape fap chicharrones. Paleo kitsch chartreuse pickled, tattooed chicharrones distillery pabst hella hexagon gentrify vaporware. Knausgaard venmo letterpress, tumblr put a bird on it squid wayfarers shabby chic pork belly butcher lomo fashion axe semiotics. Tattooed succulents poutine 3 wolf moon.", user_id: @user.id, overtime_request: 2.5)
-end
- puts "100 posts have been created"
-
 	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 6.days))
 	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 13.days))
 	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 20.days))
@@ -32,4 +26,11 @@ end
 
 
 	puts "3 audit logs have been created"
+
+
+100.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} XOXO pour-over food truck, slow-carb four dollar toast meh keytar banh mi hot chicken. Hell of umami narwhal hashtag, edison bulb normcore organic sartorial roof party DIY aesthetic fap mlkshk taxidermy cred. Photo booth mlkshk meditation distillery, succulents cred hella pork belly food truck. Keytar hashtag skateboard, occupy shabby chic post-ironic pour-over yr pickled edison bulb everyday carry vape fap chicharrones. Paleo kitsch chartreuse pickled, tattooed chicharrones distillery pabst hella hexagon gentrify vaporware. Knausgaard venmo letterpress, tumblr put a bird on it squid wayfarers shabby chic pork belly butcher lomo fashion axe semiotics. Tattooed succulents poutine 3 wolf moon.", user_id: @user.id, overtime_request: 2.5)
+end
+ puts "100 posts have been created"
+
  
