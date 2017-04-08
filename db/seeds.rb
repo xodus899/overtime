702b365f557148b372988369663cef110bafc49e
Christopher Justice <justcd@mail.broward.edu>
2017-04-08 09:35:19 -0400

    style pending audit

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,12 @@ puts "1 admin_user created"
 end
  puts "100 posts have been created"
 
-100.times do |audit_log|
 	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 6.days))
-end
+	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 13.days))
+	AuditLog.create!(user_id: @user.id, status: 0 , start_date: (Date.today - 20.days))
 
-	puts "100 audit logs have been created"
+
+
+
+	puts "3 audit logs have been created"
  
