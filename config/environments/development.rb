a1c832bc718c6b836508228bce615631b9a9ead5
Christopher Justice <justcd@mail.broward.edu>
2017-04-08 10:30:28 -0400

    manager email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,11 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -26,9 +30,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
